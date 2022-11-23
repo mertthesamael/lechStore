d640efe6d7a15ab8830b8c678138f90ab43c5628
Merto <mertenercan@gmail.com>
2022-11-23 04:32:49 +0300

    -added metamask connection

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,60 @@
-import { useContext } from "react";
+import { Image, Text } from "@chakra-ui/react";
+import { ethers } from "ethers";
+import { useContext, useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { LechContext } from "../../store/context";
-import styles from "./navbar.module.scss"
-
-
+import styles from "./navbar.module.scss";
 
 const Navbar = () => {
-    const ctx = useContext(LechContext)
-    const menuHandler = () => {
-        return(ctx.onMenuState(!ctx.menuState))
-    }
+  const ctx = useContext(LechContext);
+  const menuHandler = () => {
+    return ctx.onMenuState(!ctx.menuState);
+  };
 
-    return(
+  const connect = async () => {
+    
+    ctx.web3Connect()
+   
+  }
+
+  //useEffect hook for closing menu on blur
+  useEffect(() => {
+    const startEvent = (e) => {
+      if (e.path[0].className.split(" ")[1] !== "menuicon") {
+        return ctx.onMenuState(false);
+      } else {
+        return ctx.onMenuState(!ctx.menuState);
+      }
+    };
+    document.body.addEventListener("click", startEvent);
+    return () => {
+      document.body.addEventListener("click", startEvent);
+    };
+  }, []);
+  return (
     <header className={styles.navbar}>
-        <NavLink to='/' className={styles.navbar__logo}>
-            <img alt='lechlogo' src={require("../../assets/Save the Arts 1.png")}/>
-        </NavLink>
-        <div onClick={menuHandler} className={styles.navbar__menu}>
-        <div className={styles.navbar__menu__bar}>
+      <div onClick={menuHandler} className={styles.navbar__menu}>
+        <div className={styles.navbar__menuwrapper + " menuicon"}>
+        <div className={styles.navbar__menu__bar + " menuicon"}>
 
         </div>
         </div>
+      </div>
+      <NavLink to="/" className={styles.navbar__logo}>
+        <img alt="lechlogo" src={require("../../assets/Save the Arts 1.png")} />
+      </NavLink>
+      <div className={styles.navbar__connect}>
+        <div onClick={connect} className={styles.navbar__connect__button}>
+            <div className={styles.navbar__connect__button__icon}>
+                <img src={require("../../assets/metamask.png")}/>
+            </div>
+            <div className={styles.navbar__connect__button__text}>
+            <h1>{ctx.userAddr? ctx.userAddr :'Connect'}</h1>
+            </div>
+        </div>
+      </div>
     </header>
-    )
-}
+  );
+};
 
 export default Navbar;
--- src/store/context.js
+++ src/store/context.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { ethers } from "ethers";
+import React, { useEffect, useState } from "react";
 
 
 const LechContext = React.createContext({
@@ -10,7 +11,29 @@ export const LechContextWrapper = (props) => {
 
 
     const [menuState, setMenuState] = useState(false)
-  
+    const [connected, setConnected] = useState(false)
+    const [userAddr, setUserAddr] = useState("")
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+   
+   //Main web3 connection function
+    const web3Connect = async () => {
+        await provider.send("eth_requestAccounts", []);
+        let accounts = await provider.send("eth_requestAccounts", []);
+        let account = accounts[0];
+        
+        const connectedAddress = await signer.getAddress()
+        setUserAddr(accounts[0])
+        setConnected(true)
+    }
+
+    //Setting current addr on change
+    useEffect(() => {
+        web3Connect()
+    },[provider.on('accountsChanged')])
+
+
     const menuStateHandler = (value) => {
         return setMenuState(value)
     }
@@ -19,6 +42,9 @@ export const LechContextWrapper = (props) => {
         <LechContext.Provider value={{
             menuState:menuState,
             onMenuState:menuStateHandler,
+            web3Connect:web3Connect,
+            userAddr:userAddr,
+            connected:connected
             }}>
 
             {props.children}
